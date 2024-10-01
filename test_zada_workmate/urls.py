7a56560f17770dc461da75154206ef793857feaf
Pavlov Danil <danpavkzn@mail.ru>
2024-10-02 00:51:45 +0300

    urls

--- test_zada_workmate/urls.py
+++ test_zada_workmate/urls.py
@@ -8,7 +8,7 @@ from cats.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/catlist', CatAPIList.as_view()),
-    path('api/catlist/<int:pk>', CatAPIList.as_view()),
+    path('api/catlist/<int:pk>', APIView.as_view()),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
